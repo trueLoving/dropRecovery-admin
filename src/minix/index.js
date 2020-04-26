6a46f8d8113c8cefb7158ed52273d03b0ad7a637
star sky <1176789241@qq.com>
2020-04-26 10:45:50 +0800

    mixins 初始化

---
+++ src/minix/index.js
@@ -0,0 +1,19 @@
+let tableMixin = {
+    created(){
+        // console.log('created');
+    },
+    mounted(){
+        // console.log('mounted');
+    },
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+
+    },
+}
+
+
+export default tableMixin;
